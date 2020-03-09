f5770ae4bd465299eaa2ef107f4470c9da1f157e
Serhii Stepanchuk <serjukst@gmail.com>
2020-03-09 12:57:15 +0200

    feat: add basic solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets, newStr;
+
+  for (let i = 0; i < bracketsConfig.length; i++ ) {
+    brackets = bracketsConfig[i].join('');
+    newStr = str.replace(brackets, '');
+    
+    while (newStr.indexOf(brackets) != -1) {
+      newStr = newStr.replace(brackets, '');
+      };
+  }
+  return newStr ? false : true ;
 }
